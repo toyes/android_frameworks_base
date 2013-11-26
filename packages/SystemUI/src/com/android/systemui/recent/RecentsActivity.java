65b8e30425c7b63d0eee3b0001c323552da720d7
beerbong <yghazi@gmail.com>
2013-11-26 09:47:55 +0100

    [2/2] Frameworks: HALO
    Forward port from jb.
    PS1:  Initial port
          Fix conficts with notification LED
          Fix derp
          Major derp
          HALO Mods
          Rebase
          Fix blackist, code clean and added some TODOs
    PS2:  Fix build
    PS3:  Fix bubble sometimes doesn't respond on new notification
    PS4:  Align halo icon
    PS5:  Keep floating app in recents and disable transparent layer temporarily
    PS6:  Fix conflict after recent merges
    PS7:  Cosmetic fixes:
          Fix buttons layout in status bar.
          Status bar button holders show halo + qs switch on notification and qs edit mode + notification switch on qs.
          Update new margins size.
    PS8:  Rebase
          Clean white spaces
          Toggle on/off from notification shade icon (remove red X)
          Standardize behavior (remove option to reverse tap/double tap)
    PS9:  Remove references to LayerActivity
    PS14: Rebase
    PS15: update halo strings to prevent conflict with latest changes
    PS16: Wrap all things!
          Modify halo behavior
    PS17: Get the correct activity stack: this makes all windows in the same stack to get the floating mode
    PS18: Stop pop-out on rotation
          Catch "silent" notifications
    PS19: Rebase
    PS20: Rebase, fix conflicts with recent merges
    PS21: Fix missing commit messages from previous undefined patchset (19) and other conflicts
    
    Change-Id: I3e79945433d61da090ca263f4e2989be9987f11d
    Conflicts:
    	packages/SystemUI/res/values/pa_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsActivity.java
@@ -54,6 +54,7 @@ public class RecentsActivity extends Activity {
     private static boolean mShowing;
     private IntentFilter mIntentFilter;
     private boolean mForeground;
+    protected boolean mBackPressed;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -163,7 +164,12 @@ public class RecentsActivity extends Activity {
 
     @Override
     public void onBackPressed() {
-        dismissAndGoBack();
+        mBackPressed = true;
+        try {
+            dismissAndGoBack();
+        } finally {
+            mBackPressed = false;
+        }
     }
 
     public void dismissAndGoHome() {
@@ -187,6 +193,7 @@ public class RecentsActivity extends Activity {
                             ActivityManager.RECENT_IGNORE_UNAVAILABLE);
             if (recentTasks.size() > 1 &&
                     mRecentsPanel.simulateClick(recentTasks.get(1).persistentId)) {
+                finish();
                 // recents panel will take care of calling show(false) through simulateClick
                 return;
             }
